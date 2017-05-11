205db1523ee23cca7606236ba5a393686f1c5274
xiaotuni <xiaotuni@gmail.com>
2017-05-11 23:53:37 +0800

    *ngFor循环

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersComponent implements OnInit {
 
-  obj = { id: 1, name: 'liaohaibing', address: '我也不知道这是什么地方了' };
+  obj: object = { id: 1, name: 'liaohaibing', address: '我也不知道这是什么地方了' };
+  list: Array<any> = [];
 
-  constructor() { }
+  constructor() {
+
+    for (let i = 0; i < 10; i++) {
+      this.list.push({ id: i + 1, name: '这是名称_' + 1, address: '这是地址了——' + i });
+    }
+  }
 
   ngOnInit() {
   }
